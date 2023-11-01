813b8907c79a29b3e1934c6a98214b034da67d30
GitMarkPaul <markpaulcaneda1997@gmail.com>
2023-11-01 19:50:55 +0800

    Updates

--- assets/index.292b201c.js
+++
@@ -1,1 +0,0 @@
-import{u as s}from"./vue.f36acd1f.a32460bf.js";import{j as u}from"./entry.c5e11a5d.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return s({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=u({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};
