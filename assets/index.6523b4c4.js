81b46670402dbb37f9fba053ea1740aea48c0050
GitMarkPaul <markpaulcaneda1997@gmail.com>
2023-11-18 22:56:27 +0800

    Updates

--- assets/index.6523b4c4.js
+++
@@ -1,1 +0,0 @@
-import{u,A as l}from"./entry.c9029e4d.js";function f(e,a){const{title:r,titleTemplate:n,...o}=e;return u({title:r,titleTemplate:n,_flatMeta:o},{...a,transform(t){const s=l({...t._flatMeta});return delete t._flatMeta,{...t,meta:s}}})}export{f as u};
